75a3d1f77be01682976c4f28e603c5589a586a76
fDevJc <fDevJc@gmail.com>
2021-02-07 21:46:45 +0900

    add multer module

--- routes/test.js
+++ routes/test.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const multer = require('multer');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  console.log('test router execute');
-  res.send('Hello Test');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
+
+try {
+  router.post('/', upload.single('asb'), (req, res) => {
+    console.log('test router execute');
+    res.send('Hello Test');
+  });
+} catch (err) {
+  console.log(err);
+}
 
 module.exports = router;
