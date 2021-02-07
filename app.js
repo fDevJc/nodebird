aab82e1de47edf5effd2a15b15d2680ea97e3433
fDevJc <fDevJc@gmail.com>
2021-02-07 10:11:17 +0900

    add html,css

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express'); //express server
-//const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const morgan = require('morgan'); //log
 const path = require('path');
-//const session = require('express-session');
+const session = require('express-session');
 const nunjucks = require('nunjucks');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pageRouter = require('./routes/page');
-const testRouter = require('./routes/test');
 
 const app = express();
 
@@ -20,8 +21,36 @@ nunjucks.configure('views', {
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
 app.use('/', pageRouter);
-app.use('/test', testRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.log('app/err : ', err);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'prodcution' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), 'port server running.....');
--- routes/page.js
+++ routes/page.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내정보 - NodeBird' });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+});
+
 router.get('/', (req, res) => {
-  res.render('main');
+  console.log('page / router');
+  const twits = [];
+  res.render('main', { title: 'NodeBird', twits });
 });
 
 module.exports = router;
